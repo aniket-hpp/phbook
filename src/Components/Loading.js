1e48b67b8785a4d339dd73aa1a955530da5b2557
Aniket Biswas <97422997+thesmartaniket@users.noreply.github.com>
2023-07-13 17:35:20 +0530

    Update Loading.js

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -14,7 +14,7 @@ const Loading = ({loading}) => {
             position: "fixed",
             background: "rgba(255, 255, 255, 0.7)",
             backdropFilter: "blur(5px)",
-            overflow-y: 'hidden'
+            overflow-y: "hidden",
         }}>
             <Icon icon={LoadingIcon} display={true}/>
         </div>
